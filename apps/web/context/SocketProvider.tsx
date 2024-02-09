40b55a93a7be925f2a99c3a60ae3c23ce2f0c51e
Pranav Sambyal <singhsambyalpranav@gmail.com>
2024-02-09 21:40:58 +0530

    tried to Fixed io socket

--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -38,7 +38,7 @@ export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
   },[])
 
   useEffect(() => {
-    const _socket = io('https://scaleable-chat-app-docs.vercel.app/');
+    const _socket = io('http://localhost');
     _socket.on('message', onMessageRec);
     setSocket(_socket);
     return () => {
